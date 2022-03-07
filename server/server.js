a4402e4864d6bf44bcd83674b9a8e753a88318e4
rakib2067 <rakib7@hotmail.co.uk>
2022-03-07 17:41:24 +0000

    Server side Read, Write and Update working

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = 4000;
+
+app.use(express.json());
+app.use(cors());
+app.listen(port, () => {
+  console.log(`Rook app listening on port ${port}`);
+});
+
+const gifController = require("./controllers/controller");
+app.use("/", gifController);
