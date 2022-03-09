6d7e73ec3c8c93746e6c918e52f2570a2945578d
rakib2067 <rakib7@hotmail.co.uk>
2022-03-09 16:34:07 +0000

    Giphy Testing Done

---
+++ client/test/app.test.js
@@ -0,0 +1,40 @@
+const fs = require("fs");
+const path = require("path");
+const html = fs.readFileSync(path.resolve(__dirname, "../index.html"), "utf8");
+
+global.fetch = require("jest-fetch-mock");
+
+let app;
+let mainApp;
+
+describe("Testing App Functionality", () => {
+  beforeEach(() => {
+    document.documentElement.innerHTML = html.toString();
+    app = require("../js/Giphy.js");
+    mainApp = require("../js/Main.js");
+  });
+
+  afterEach(() => {
+    fetch.resetMocks();
+  });
+  describe("Giphy API", () => {
+    test("Fetch from API", () => {
+      app.getTrending();
+      expect(fetch.mock.calls[0][0]).toMatch(
+        /https:\/\/api.giphy.com\/v1\/gifs\/trending\?api_key=8PiyixOfCPFFExgTLW5347Y8xbuMoYGk&limit=45&rating=g/
+      );
+    });
+    test("Fetch Specific from the API", () => {
+      app.fetchGifs();
+      expect(fetch.mock.calls[0][0]).toMatch(
+        /https:\/\/api.giphy.com\/v1\/gifs\/search\?api_key=8PiyixOfCPFFExgTLW5347Y8xbuMoYGk&q=&limit=25&offset=0&rating=g&lang=en/
+      );
+    });
+    test("Reset Search", () => {
+      app.resetSearch();
+      expect(fetch.mock.calls[0][0]).toMatch(
+        /https:\/\/api.giphy.com\/v1\/gifs\/trending\?api_key=8PiyixOfCPFFExgTLW5347Y8xbuMoYGk&limit=45&rating=g/
+      );
+    });
+  });
+});
