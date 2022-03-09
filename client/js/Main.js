f5fbfcb777d69d286fefbeea23d12e54ac94b92f
rakib2067 <rakib7@hotmail.co.uk>
2022-03-09 12:05:28 +0000

    Cleaned up Code

--- client/js/Main.js
+++ client/js/Main.js
@@ -1,10 +1,11 @@
+let Giphy = require("./Giphy.js");
+
 let title = document.querySelector(".post-title");
 let description = document.querySelector(".post-description");
 
 // Loading all the Cards from Backend
 window.onload = initLoad;
 
-let loaded = false;
 function initLoad() {
   fetch("http://localhost:4000/")
     .then((res) => res.json())
@@ -12,16 +13,13 @@ function initLoad() {
       try {
         let templateData = data;
         //
-        //
-        //
-        //
         // Getting Comment
         try {
           for (let post of templateData) {
             fetch(`http://localhost:4000/comment/${post.id}`)
               .then((res) => res.json())
               .then((data) => {
-                console.log(data);
+                //console.log(data);
                 if (data[0].comment) {
                   post["comments"] = data[0].comment;
                 } else {
@@ -32,11 +30,6 @@ function initLoad() {
         } catch (e) {
           console.log("No Posts", e);
         }
-        console.log(templateData);
-
-        //
-        //
-        //
         //
         setTimeout(() => {
           let template = Handlebars.compile(
@@ -53,92 +46,8 @@ function initLoad() {
     });
 }
 
-// GIPHY API
-
-let gif = document.querySelector("#post-gif");
-let gallery = document.querySelector(".gifs");
-let gif_search = document.querySelector("#gif-search");
-let gif_reset = document.querySelector("#gif-reset");
-let selectedGif;
-getTrending();
-function getTrending() {
-  fetch(
-    "https://api.giphy.com/v1/gifs/trending?api_key=8PiyixOfCPFFExgTLW5347Y8xbuMoYGk&limit=45&rating=g"
-  )
-    .then((res) => {
-      return res.json();
-    })
-    .then(({ data }) => {
-      data.forEach((gif) => {
-        let image = document.createElement("img");
-        image.src = gif.images["fixed_height_small"].url;
-        image.classList.add("gif-image");
-        gallery.append(image);
-      });
-      addGifHandlers();
-    })
-    .catch((error) => {
-      alert("Error: ", error);
-    });
-}
-
-gif_search.addEventListener("click", fetchGifs);
-
-function fetchGifs() {
-  let userGif = gif.value;
-  gallery.innerHTML = "";
-  fetch(
-    `https://api.giphy.com/v1/gifs/search?api_key=8PiyixOfCPFFExgTLW5347Y8xbuMoYGk&q=${userGif}&limit=25&offset=0&rating=g&lang=en`
-  )
-    .then((res) => {
-      return res.json();
-    })
-    .then(({ data }) => {
-      data.forEach((gif) => {
-        let image = document.createElement("img");
-        image.src = gif.images["fixed_height_small"].url;
-        image.classList.add("gif-image");
-        gallery.append(image);
-      });
-      addGifHandlers();
-    })
-    .catch((error) => {
-      alert("Error: ", error);
-    });
-}
-
-gif_reset.addEventListener("click", resetSearch);
-
-function resetSearch() {
-  gallery.innerHTML = "";
-  getTrending();
-  gif.value = "";
-}
-
-// Looping thorugh all gifs to handle selection
-
-function addGifHandlers() {
-  let selected = false;
-
-  let gif_images = document.querySelectorAll(".gif-image");
-  gif_images.forEach((gif) => {
-    gif.addEventListener("click", () => {
-      if (!selected) {
-        selected = true;
-        selectedGif = gif;
-        selectedGif.style.border = "3px solid red";
-      } else {
-        selectedGif.style.border = "none";
-        selectedGif = gif;
-        selectedGif.style.border = "3px solid red";
-      }
-    });
-  });
-}
-
 // Form Submit
 let form = document.querySelector(".create-post");
-
 form.addEventListener("submit", returnUserInput);
 
 //store input
@@ -146,13 +55,13 @@ function returnUserInput(e) {
   e.preventDefault();
   let userTitle = title.value;
   let userDescription = description.value;
-  if (!selectedGif) {
+  if (!Giphy.selectedGif) {
     return alert("Must Select a valid GIF");
   }
   let data = {
     title: userTitle,
     content: userDescription,
-    giphy: selectedGif.src,
+    giphy: Giphy.selectedGif.src,
     emo1: 0,
     emo2: 0,
     emo3: 0,
@@ -175,7 +84,6 @@ function returnUserInput(e) {
 }
 
 // Store comments
-
 function addCardHandler() {
   let cards = document.querySelectorAll(".card");
 
@@ -186,38 +94,36 @@ function addCardHandler() {
 
     // Emoji Form
     let emojiCont = card.lastElementChild.firstElementChild.children;
-
     for (let emoji of emojiCont) {
-      let data = {
-        id: card.id,
-        emo: emoji.id,
-      };
-      emoji.addEventListener("click", () => {
-        //{id, emo1, emo2, emo3}
-        console.log("Emoji: ", emoji, emoji.textContent);
-        fetch("http://localhost:4000/emo", {
-          method: "POST",
-          body: JSON.stringify(data),
-          headers: {
-            "Content-Type": "application/json",
-          },
-        })
-          .then((res) => {
-            console.log(res);
-          })
-          .catch((e) => {
-            alert(e);
-          });
-      });
+      emoji.addEventListener("click", emoteHandler);
     }
   });
+}
+
+function emoteHandler(e) {
+  let data = {
+    id: e.target.parentElement.parentElement.parentElement.id,
+    emo: e.target.id,
+  };
+  fetch("http://localhost:4000/emo", {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((res) => {
+      console.log(res);
+    })
+    .catch((e) => {
+      alert(e);
+    });
 }
 
 function commentHandler(e) {
   e.preventDefault();
   console.log(e);
   let id = e.target.parentElement.parentElement.id;
-  let emojiContainer = e.target.parentElement.firstElementChild;
 
   let input = e.target[0].value;
   let data = {
