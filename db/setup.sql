11e254d1e34dedadf36f162797caaf862a31e100
rakib2067 <rakib7@hotmail.co.uk>
2022-03-21 09:47:30 +0000

    postgres setup

--- db/setup.sql
+++ db/setup.sql
@@ -1,13 +1,32 @@
+DROP TABLE IF EXISTS posts;
 CREATE TABLE posts(
-    postID ID SERIAL PRIMARY KEY
+    postID serial PRIMARY KEY,
+    title varchar(100),
+    content varchar(200) NOT NULL,
+    giphy varchar(200),
+    emo1 INT,
+    emo2 INT,
+    emo3 INT
+);
 
-)
+DROP TABLE IF EXISTS comments;
+CREATE TABLE comments(
+    commentID serial PRIMARY KEY,
+    timeOfPost timestamp ,
+    input varchar(250),
+    postID int
+);
 
---    this.id = data.id;
---     this.title = data.title;
---     this.content = data.content;
---     this.giphy = data.giphy;
---     this.emo1 = data.emo1 || 0;
---     this.emo2 = data.emo2 || 0;
---     this.emo3 = data.emo3 || 0;
---     this.comment = [];
+
+INSERT into posts(title, content,giphy,emo1,emo2,emo3)
+VALUES
+   ('test1','Test Description','https://media1.giphy.com/media/mznXgdZkk8KA2GMb2e/100.gif?cid=1253b2878qz6ziqwwjzgp3963qgce16v6xoum496iy7jp4y3&rid=100.gif&ct=g',1,0,0);
+
+INSERT into comments(timeOfPost,input,postID)
+VALUES (CURRENT_TIMESTAMP, 'Test Description',1);
+
+
+
+SELECT * from posts;
+-- insert into example(arr) values('{1, 2, 3}');
+
